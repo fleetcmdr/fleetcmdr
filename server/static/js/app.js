3e02517f60f05635306a04699186b4f679efb2b9
Brendan Porter <bporter@infinitydirect.com>
2025-01-04 18:38:48 -0600

    fixes poor animation of activity progress bars

--- server/static/js/app.js
+++ server/static/js/app.js
@@ -4,7 +4,7 @@ AgentData.ID = 0;
 
 function updateCPUChart(thing) {
     // console.log(thing)
-    $('#cpu_activity .progress-bar').animate({width: thing.cpu + "%"}, 250);
+    $('#cpu_activity .progress-bar').animate({width: (thing.cpu/100) * $('#cpu_activity').width()}, 1000);
 
     if(thing.cpu < 80){
         $('#cpu_activity .progress-bar').removeClass('bg-warning').removeClass('bg-critical').addClass('bg-success')
@@ -19,7 +19,7 @@ function updateCPUChart(thing) {
 
 function updateRAMChart(thing) {
     // console.log(thing)
-    $('#ram_pressure .progress-bar').animate({width: thing.ram + "%"}, 250);
+    $('#ram_pressure .progress-bar').animate({width: (thing.ram/100) * $('#ram_pressure').width()}, 1000);
 
     if(thing.ram < 80){
         $('#ram_pressure .progress-bar').removeClass('bg-warning').removeClass('bg-critical').addClass('bg-success')
@@ -35,7 +35,7 @@ function updateRAMChart(thing) {
 function updateDiskChart(thing) {
     // console.log(thing)
 
-    $('#disk_consumed .progress-bar').animate({width: thing.disk + "%"}, 250);
+    $('#disk_consumed .progress-bar').animate({width: (thing.disk/100) * $('#disk_consumed').width()}, 250);
 
     if(thing.disk < 80){
         $('#disk_consumed .progress-bar').removeClass('bg-warning').removeClass('bg-critical').addClass('bg-success')
